3389dc720e49b549f93b9751bbc4bb156f1c018e
Peter Kabai <peterkabai@MacBookPro.local>
2019-12-02 11:53:27 -0800

    Adds day 1

---
+++ 2019/day_1_part_1.py
@@ -0,0 +1,11 @@
+# https://adventofcode.com/2019/day/1
+
+import math
+
+total_fuel = 0
+for line in open('day_1_input.txt'):
+    mass = int(line.rstrip('\n'))
+    fuel = math.floor(mass/3)-2
+    total_fuel += fuel
+
+print(total_fuel)
---
+++ 2019/day_1_part_2.py
@@ -0,0 +1,12 @@
+# https://adventofcode.com/2019/day/1
+
+import math
+
+total_fuel = 0
+for line in open('day_1_input.txt'):
+    mass = int(line.rstrip('\n'))
+    while mass > 0:
+        mass = math.floor(mass/3)-2
+        total_fuel += mass if mass > 0 else 0
+
+print(total_fuel)
